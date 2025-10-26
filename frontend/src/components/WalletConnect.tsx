922067f036fe5589dd80906904826e5700b04aac
archie40004 <archie40004@gmail.com>
2025-10-26 16:37:40 +0000

    frontend changes

---
+++ frontend/src/components/WalletConnect.tsx
@@ -0,0 +1,10 @@
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const WalletConnect = () => {
+  return (
+    <WalletMultiButton />
+  );
+};
+
+export default WalletConnect;
